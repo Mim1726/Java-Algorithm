ba86f31d38073bb23745aa340ddeeca3ebd02174
Suraya Jannat Mim <150699804+Mim1726@users.noreply.github.com>
2025-05-16 10:51:31 +0600

    Create kruskal2.java

---
+++ minimum spanning tree/kruskal2.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+
+    Edge(int s, int d, int w) {
+        src = s;
+        dest = d;
+        weight = w;
+    }
+
+    public int compareTo(Edge other) {
+        return this.weight - other.weight;
+    }
+}
+
+class DisjointSet {
+    int[] parent, rank;
+
+    DisjointSet(int n) {
+        parent = new int[n];
+        rank = new int[n];
+        for(int i = 0; i < n; i++)
+            parent[i] = i;
+    }
+
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);  // Path compression
+        return parent[x];
+    }
+
+    boolean union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX == rootY)
+            return false;
+
+        if (rank[rootX] < rank[rootY])
+            parent[rootX] = rootY;
+        else if (rank[rootX] > rank[rootY])
+            parent[rootY] = rootX;
+        else {
+            parent[rootY] = rootX;
+            rank[rootX]++;
+        }
+        return true;
+    }
+}
+
+public class KruskalsAlgorithm {
+
+    public static int kruskalMST(int n, List<Edge> edges) {
+        Collections.sort(edges);
+        DisjointSet ds = new DisjointSet(n);
+
+        int totalCost = 0;
+        System.out.println("Edges in the Minimum Spanning Tree:");
+        for (Edge edge : edges) {
+            if (ds.union(edge.src, edge.dest)) {
+                totalCost += edge.weight;
+                System.out.println(edge.src + " - " + edge.dest + " : " + edge.weight);
+            }
+        }
+        return totalCost;
+    }
+
+    public static void main(String[] args) {
+        int n = 4; // number of vertices
+        List<Edge> edges = new ArrayList<>();
+        edges.add(new Edge(0, 1, 10));
+        edges.add(new Edge(0, 2, 6));
+        edges.add(new Edge(0, 3, 5));
+        edges.add(new Edge(1, 3, 15));
+        edges.add(new Edge(2, 3, 4));
+
+        int cost = kruskalMST(n, edges);
+        System.out.println("Total cost of MST: " + cost);
+    }
+}
